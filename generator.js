c57c917556e254ea29caf8a6578096464a0e7030
jay698 <63207332+jay698@users.noreply.github.com>
2020-08-02 22:22:03 +0200

    Add files via upload

---
+++ generator.js
@@ -0,0 +1,11 @@
+/*
+let T1 = 1;
+
+if (T1 < 20, T1++) {
+    Ans = 1/Math.sqrt(5)*(((1+Math.sqrt(5)/2).Math.pow(T1))-((1+Math.sqrt(5)/2).Math.pow(T1)))/Math.sqrt(5);
+    return Ans
+} else() => {
+
+}
+*/
+
